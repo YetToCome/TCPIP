e008f714bc0bae8409fd1fbbcd3b4682294c8f85
YetToCome <wjw15129@gmail.com>
2014-04-09 20:21:20 +0800

    tcpserver

--- TCP_1/skel.h
+++ TCP_1/skel.h
@@ -8,9 +8,9 @@
 #ifndef __SKEL_H__
 #define __SKEL_H__
 
-#define INIT() ( program_name = strrchr( argv[0], '/' )) ? program_name++: (program_name = agrv[0])
+#define INIT() ( program_name = strrchr( argv[0], '/' )) ? program_name++: (program_name = argv[0])
 #define EXIT(s) exit(s)
-#define CLOSE() if ( close( s )) error(1, error, "close failed")
+#define CLOSE(s) if ( close( s )) error(1, error, "close failed")
 #define set_errno(e) error = (e)
 #define isvalidsock(s) ((s) >= 0)
 
